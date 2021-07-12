88f02c633b85d46b60d42859fdd82b511326249f
William Claster <william108@gmail.com>
2021-07-12 09:35:47 +0900

    one section of 531

--- R/create.R
+++ R/create.R
@@ -1,4 +1,7 @@
 usethis::use_tutorial("Exercises524", "Exercises 5.2.4", open = interactive())
 
+usethis::use_tutorial("Exercises5.3.1", "Exercises 5.3.1", open = interactive())
+
+
 #Onto Github
 devtools::install_github("DataScienceProjectsJapan/ExercisesForHadleyDS")
